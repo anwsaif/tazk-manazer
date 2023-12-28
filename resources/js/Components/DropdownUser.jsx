bfe086d6c258beaf7622c3842aba06b937351a2c
Anwar S <anwarxsaifullah+git@gmail.com>
2023-12-28 06:51:54 +0000

    Add logout template

--- resources/js/Components/DropdownUser.jsx
+++ resources/js/Components/DropdownUser.jsx
@@ -154,7 +154,13 @@ const DropdownUser = () => {
             </Link>
           </li>
         </ul>
-        <button className="flex items-center gap-3.5 px-6 py-4 text-sm font-medium duration-300 ease-in-out hover:text-primary lg:text-base">
+
+        <Link
+          href={route('logout')}
+          method="post"
+          as="button"
+          className="flex items-center gap-3.5 px-6 py-4 text-sm font-medium duration-300 ease-in-out hover:text-primary lg:text-base"
+        >
           <svg
             className="fill-current"
             width="22"
@@ -173,7 +179,7 @@ const DropdownUser = () => {
             />
           </svg>
           Log Out
-        </button>
+        </Link>
       </div>
       {/* <!-- Dropdown End --> */}
     </div>
